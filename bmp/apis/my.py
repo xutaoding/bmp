6467ab6e3a5dce924e3a18894c7b03639e61fd99
chenglong.yan <chenglong.yan@chinascopefinancial.com>
2015-10-12 09:10:13 +0800

    .

--- bmp/apis/my.py
+++ bmp/apis/my.py
@@ -3,7 +3,7 @@ from bmp.apis.base import BaseApi
 from flask import session
 from bmp.const import USER_SESSION
 
-class UserApi(BaseApi):
-    route="/user"
+class MyApi(BaseApi):
+    route="/my"
     def get(self):
         return self.succ(session[USER_SESSION])
