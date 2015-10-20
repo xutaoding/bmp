f3299348461fbeaed4a96cf7d588e9f8190cdedb
root <root@sh-192-168-250-10.chinascopefinancial.com>
2015-10-20 09:42:50 +0800

    .

--- runserver.py
+++ runserver.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 #coding: utf-8
 
 '''
@@ -5,6 +6,14 @@ git clone git@gitlab.chinascope.net:web/ops.git
 ln -s ops/static/static/ bmp/static/
 ln -s ops/static/templates/ bmp/templates/
 '''
+import sys
 
-from bmp import app
-app.run(host="192.168.0.143",port=5000,debug=True)
+sys.path.append("/var/www/scope/bmp")
+
+
+
+from bmp import app as application
+
+
+if __name__=="__main__":
+    application.run(host="0.0.0.0",port=5000,debug=True)
