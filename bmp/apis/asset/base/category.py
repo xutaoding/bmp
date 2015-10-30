c5f6088367f7f8d890d37169bb73a47eeaa5b209
yara.yu <yara.yu@chinascopefinancial.com>
2015-10-30 10:21:53 +0800

    .

--- bmp/apis/asset/base/category.py
+++ bmp/apis/asset/base/category.py
@@ -1,46 +1,47 @@
+# coding: utf-8
 from bmp import db
 from bmp.apis.base import BaseApi
+from bmp.models.asset import Category
 
 
 class CategoryApi(BaseApi):
-    route = ["/asset/base/category/devicetype/<int:id>", "/asset/base/category/devicename/<int:id>",
-             "/asset/base/category/elaborate/<int:id>"]
+    route = ["/asset/base/category/<int:id>"]
     # route=["/asset/base/category"]
 
     def auth(self):
         return True
 
-    def get(self, id=0):pass
-        #return self.succ(DeviceType.history())
+    def get(self, parent_id=0):
+        return self.succ(Category.history(parent_id))
 
-    def post_devicetype(self):
+    def post(self):
         submit = self.request()
-
+        Category.add(submit)
         return self.succ()
 
-    def post_devicename(self):
+    def delete(self, id):
+        Category.delete(id)
+        return self.succ()
+
+
+    def put(self, id):
+        print 'ss:', id
         submit = self.request()
-        #DeviceName.add(submit)
+        Category.edit(id, submit)
         return self.succ()
 
-    def post_elaborate(self):
-        submit = self.request()
-        #Elaborate.add(submit)
-        return self.succ()
 
-    def delete_devicetype(self, id):
-        #DeviceType.delete(id)
-        #DeviceName.delete()
-        return self.succ()
+if __name__ == "__main__":
+    from bmp.utils.post import test
 
-    def delete_devicename(self, id):
-        #DeviceName.delete(id)
-        return self.succ()
+    test(
+        "delete",
+        "http://192.168.0.57:5000/apis/v1.0/asset/base/category/3",
+        {
+            "id": "3",
+            "name": "路由器",
+            "parent_id": "2"
+        },
+        True
 
-    # def delete_elaborate(self, id):
-    #     Elaborate.delete(id)
-        # return self.succ()
-
-
-
-
+    )
