d489fae63af7963b63393abf3507d97f5c736975
chenglong.yan <chenglong.yan@chinascopefinancial.com>
2016-01-25 18:33:32 +0800

    .

---
+++ bmp/tasks/mail/base.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+from flask import render_template
+import bmp.utils.mail as mail
+import re
+from flask import request
+
+class BaseMail:
+    def __init__(self):
+        pass
+
+    def send(self,to,sub,url,tpl,**kwargs):
+
+        regx = re.compile(r"^http://([a-z.]+)/")
+        host = regx.findall(request.headers["Referer"])[0]
+
+        if "dev" in host: sub = u"【测试】 %s" % sub
+
+        kwargs["url"] = "http://%s%s" % (host,url)
+        kwargs["sub"] = sub
+
+        html = render_template(tpl,**kwargs)
+
+        mail.send(sub,html,list(set(to)))
--- bmp/tasks/mail/leave.py
+++ bmp/tasks/mail/leave.py
@@ -9,31 +9,29 @@ from bmp.const import DEFAULT_GROUP, LEAVE
 from bmp.models.user import User, Group
 from bmp.models.ref import Ref
 from bmp import log
+from base import BaseMail
 
 
-def mail_to(l):
-    try:
+class Mail(BaseMail):
+    def to(self, l):
         uids = [u.uid for u in Group.get_users(DEFAULT_GROUP.HR)] + [l.approval_uid]
         to = [User.get(uid)["mail"] for uid in uids]
         to.append("hr.dept@chinascopefinancial.com")
 
         sub = u"请假申请 编号:%d 申请人:%s" % (l.id, l.uid)
 
-        regx = re.compile(r"^http://([a-z.]+)/")
-        host = regx.findall(request.headers["Referer"])[0]
-
-        if "dev" in host: sub = u"【测试】 %s" % sub
-
-        url = "http://%s/templates/leave/approval.html" % host
-
-        html = render_template(
+        self.send(
+            to,
+            sub,
+            "/templates/leave/approval.html",
             "mail.leave.tpl.html",
-            sub=sub,
             leave=l,
-            ref=Ref.map(LEAVE.TYPE),
-            url=url)
+            ref=Ref.map(LEAVE.TYPE))
 
-        mail.send(sub, html, list(set(to)))
+
+def mail_to(l):
+    try:
+        Mail().to(l)
     except Exception, e:
         log.exception(e)
 
