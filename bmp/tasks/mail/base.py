a2c6c143896d513c2902e6eddf02efd916639e39
CrazyBug <chenglong_yan@foxmail.com>
2016-03-30 11:16:58 +0800

    .

--- bmp/tasks/mail/base.py
+++ bmp/tasks/mail/base.py
@@ -4,21 +4,20 @@ import bmp.utils.mail as mail
 import re
 from flask import request
 
+
 class BaseMail:
     def __init__(self):
         pass
 
-    def send(self,to,sub,url,tpl,cc=[],date=None,**kwargs):
-
+    def send(self, to, sub, url, tpl, cc=[], date=None, **kwargs):
         regx = re.compile(r"^http://([a-z.]+)/")
         host = regx.findall(request.headers["Referer"])[0]
 
         if "dev" in host: sub = u"【测试】 %s" % sub
 
-        kwargs["url"] = "http://%s%s" % (host,url)
+        kwargs["url"] = "http://%s%s" % (host, url)
         kwargs["sub"] = sub
 
-        html = render_template(tpl,**kwargs)
+        html = render_template(tpl, **kwargs)
 
-
-        mail.send(sub,html,list(set(to)),cc,date)
+        mail.send(sub, html, list(set(to)), cc, date)
