c8f441dde1ebc7557e1a750a33b9640ee77dc420
CrazyBug <chenglong_yan@foxmail.com>
2016-03-30 11:22:36 +0800

    .

--- bmp/utils/sms.py
+++ bmp/utils/sms.py
@@ -1,33 +1,36 @@
 # coding=utf-8
 
-# todo 神盾局OPS系统：前方英雄【变量】即将在【变量】过期，请及时充能！！
-
-# 用户名:cf_chinascope
-# 密码:zKqVjm
-
-
-
-# http://106.ihuyi.cn/webservice/sms.php?method=Submit
-# 接口说明
-# 类型	参数名称	参数值描述
-# 参数	method	Submit
-# 参数	account	提交账户名
-# 参数	password	提交账户密码 （采用MD5加密）
-# 参数	mobile	接收手机号码，只能提交1个号码
-# 参数	content	短信内容（支持300个字的长短信，长短信按多条计费）
 import requests
 import xml.dom.minidom as dom
+from xml.dom.minidom import Text, Element
+from bmp import log, app
 
 
-def send(content):
-    return requests.post(
-        "http://106.ihuyi.cn/webservice/sms.php?method=Submit",
-        {
-            "account": "cf_chinascope",
-            "password": "zKqVjm"
-        }
-    ).text.decode("utf8")
+def send(mobile, content):
+    try:
+        root = dom.parseString(requests.post(
+            app.config["SMS_GATEWAY"],
+            {
+                "method": "Submit",
+                "account": app.config["SMS_USER"],
+                "password": app.config["SMS_PASSWORD"],
+                "mobile": mobile,
+                "content": content
+            }
+        ).text.encode("utf-8"))
 
-if __name__=="__main__":
-    dd=dom.parseString(send("test"))
-    print dd
+        code, msg, smsid = [
+            node.childNodes[0].data
+            for node in root.getElementsByTagName("SubmitResult")[0].childNodes
+            if isinstance(node, Element)]
+
+        if code != "2":
+            raise Exception("短信发送失败 %s %s  手机号%s 内容%s" % (code, msg, mobile, content))
+
+
+    except Exception, e:
+        log.exception(e)
+
+
+if __name__ == "__main__":
+    send("15618226618", "神盾局OPS系统：前方英雄【域名】即将在【2016-01-05】过期，请及时充能！！")
