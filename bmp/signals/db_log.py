470003a448894f278aacd8e7240f61369f64b84f
CrazyBug <chenglong_yan@foxmail.com>
2016-04-01 13:28:43 +0800

    .

--- bmp/signals/db_log.py
+++ bmp/signals/db_log.py
@@ -3,31 +3,35 @@ from flask import session
 from datetime import datetime
 import MySQLdb
 import re
+from bmp import log as _log
 
 
 def log(app, changes):
-    uri=app.config["SQLALCHEMY_DATABASE_URI"]
-    user,passwd,host,_db=re.compile("mysql://(.+):(.+)@(.+):[0-9]+/(.+)").findall(uri)[0]
+    try:
+        uri = app.config["SQLALCHEMY_DATABASE_URI"]
+        user, passwd, host, _db = re.compile("mysql://(.+):(.+)@(.+):[0-9]+/(.+)").findall(uri)[0]
 
-    if session.__contains__(USER_SESSION):
-        with MySQLdb.connect(user=user,passwd=passwd,host=host,db=_db,charset="utf8") as cursor:
-            for obj, action in changes:
-                cursor.execute(
-                    "insert into log_sqlalchemy "
-                    "(`action`,`table`,`object`,`uid`,`create_time`) VALUES (%s,%s,%s,%s,%s)",
-                    (action,
-                    obj.__class__.__name__,
-                    obj.to_dict().__str__(),
-                    session[USER_SESSION]["uid"],
-                    datetime.now())
-                )
+        if session.__contains__(USER_SESSION):
+            with MySQLdb.connect(user=user, passwd=passwd, host=host, db=_db, charset="utf8") as cursor:
+                for obj, action in changes:
+                    cursor.execute(
+                        "insert into log_sqlalchemy "
+                        "(`action`,`table`,`object`,`uid`,`create_time`) VALUES (%s,%s,%s,%s,%s)",
+                        (action,
+                         obj.__class__.__name__,
+                         obj.to_dict().__str__(),
+                         session[USER_SESSION]["uid"],
+                         datetime.now())
+                    )
+    except Exception, e:
+        _log.exception(e)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     from flask_sqlalchemy import models_committed
     from bmp import app
 
-
-    models_committed.connect(log,app)
+    models_committed.connect(log, app)
 
     from bmp.models.asset import Category
 
