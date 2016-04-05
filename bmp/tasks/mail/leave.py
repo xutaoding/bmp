37d251bee8771cc4788f4c603d0cc939815da2dc
CrazyBug <chenglong_yan@foxmail.com>
2016-04-05 14:51:18 +0800

    1.修改 邮件主题

--- bmp/tasks/mail/leave.py
+++ bmp/tasks/mail/leave.py
@@ -14,7 +14,7 @@ class Mail(BaseMail):
             uids = [l.approval_uid] + l.copy_to_uid.split(",")
             to = [User.get(uid)["mail"] for uid in uids]
 
-            sub = u"请假申请 编号:%d 申请人:%s 申请时间:%s" % (l.id, l.uid,l.apply_time.strftime("%Y-%m-%d"))
+            sub = u"请假申请 申请人:%s 申请时间:%s 编号:%d" % (l.uid,l.apply_time.strftime("%Y-%m-%d"),l.id)
 
             self.send(
                 to,
@@ -24,7 +24,7 @@ class Mail(BaseMail):
                 leave=l,
                 ref=Ref.map(LEAVE.TYPE))
         except:
-            raise ExceptionEx("邮件发送失败!")
+            raise ExceptionEx("邮件发送失败,请重试!")
 
 
 
--- bmp/tasks/mail/leave_approval.py
+++ bmp/tasks/mail/leave_approval.py
@@ -5,28 +5,33 @@ from bmp.models.user import User, Group
 from bmp.models.ref import Ref
 from bmp import log
 from base import BaseMail
+from bmp.utils.exception import ExceptionEx
 
 
 class Mail(BaseMail):
     def to(self, l):
-        uids = [l.uid, l.approval_uid] + l.copy_to_uid.split(",")
-        if l.status == LEAVE.PASS:
-            uids += [u.uid for u in Group.get_users(DEFAULT_GROUP.LEAVE.MAIL)]
-            to = [User.get(uid)["mail"] for uid in uids]
-            to.append("hr.dept@chinascopefinancial.com")
-        else:
-            to = [User.get(uid)["mail"] for uid in uids]
+        try:
+            uids = [l.uid, l.approval_uid] + l.copy_to_uid.split(",")
+            if l.status == LEAVE.PASS:
+                uids += [u.uid for u in Group.get_users(DEFAULT_GROUP.LEAVE.MAIL)]
+                to = [User.get(uid)["mail"] for uid in uids]
+                to.append("hr.dept@chinascopefinancial.com")
+            else:
+                to = [User.get(uid)["mail"] for uid in uids]
 
-        status = l.status
-        if status != LEAVE.PASS:
-            status = u"已退回"
+            status = l.status
+            if status != LEAVE.PASS:
+                status = u"已退回"
 
-        sub = u"请假申请%s 编号:%d 申请人:%s 审批时间:%s" % (status, l.id, l.uid, l.approval_time.strftime("%Y-%m-%d"))
+            sub = u"%s的请假申请%s 审批时间:%s 编号:%d" % ( l.uid,status,l.approval_time.strftime("%Y-%m-%d"),l.id)
 
-        self.send(
-            to,
-            sub,
-            "/templates/leave/approval.html",
-            "mail.leave.tpl.html",
-            leave=l,
-            ref=Ref.map(LEAVE.TYPE))
+            self.send(
+                to,
+                sub,
+                "/templates/leave/approval.html",
+                "mail.leave.tpl.html",
+                leave=l,
+                ref=Ref.map(LEAVE.TYPE))
+
+        except:
+            raise ExceptionEx("邮件发送失败,请重试!")
