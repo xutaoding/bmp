644d6b54b123403ec6f545d08ad0bcb4c433d281
CrazyBug <chenglong_yan@foxmail.com>
2016-04-05 13:36:30 +0800

    1.添加 邮件发送失败提示

--- bmp/tasks/mail/leave.py
+++ bmp/tasks/mail/leave.py
@@ -5,22 +5,26 @@ from bmp.models.user import User, Group
 from bmp.models.ref import Ref
 from bmp import log
 from base import BaseMail
+from bmp.utils.exception import ExceptionEx
 
 
 class Mail(BaseMail):
     def to(self, l):
-        uids = [l.approval_uid] + l.copy_to_uid.split(",")
-        to = [User.get(uid)["mail"] for uid in uids]
+        try:
+            uids = [l.approval_uid] + l.copy_to_uid.split(",")
+            to = [User.get(uid)["mail"] for uid in uids]
 
-        sub = u"请假申请 编号:%d 申请人:%s 申请时间:%s" % (l.id, l.uid,l.apply_time.strftime("%Y-%m-%d"))
+            sub = u"请假申请 编号:%d 申请人:%s 申请时间:%s" % (l.id, l.uid,l.apply_time.strftime("%Y-%m-%d"))
 
-        self.send(
-            to,
-            sub,
-            "/templates/leave/approval.html",
-            "mail.leave.tpl.html",
-            leave=l,
-            ref=Ref.map(LEAVE.TYPE))
+            self.send(
+                to,
+                sub,
+                "/templates/leave/approval.html",
+                "mail.leave.tpl.html",
+                leave=l,
+                ref=Ref.map(LEAVE.TYPE))
+        except:
+            raise ExceptionEx("邮件发送失败!")
 
 
 
