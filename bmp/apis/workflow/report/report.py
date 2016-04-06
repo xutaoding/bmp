4d3fae59ec836b30ba4d0e475dcc676bdf5b527b
CrazyBug <chenglong_yan@foxmail.com>
2016-04-06 16:06:41 +0800

    .

--- bmp/apis/workflow/report/report.py
+++ bmp/apis/workflow/report/report.py
@@ -17,30 +17,34 @@ class ReportApi(BaseApi):
     ]
 
     def get(self, year, weeks, team_id=None):
-        dt = datetime(year, 1, 1) + timedelta(weeks=weeks-1)
+        dt = datetime(year, 1, 1) + timedelta(weeks=weeks - 1)
         beg_time = dt - timedelta(days=dt.weekday())
         end_time = (dt + timedelta(days=6 - dt.weekday())).replace(hour=23, minute=59, second=59)
         if team_id:
-            result=Report.select(
+            result = Report.select(
                 _filters=[Report.create_time.between(beg_time, end_time), Report.team_id == team_id])
         else:
-            result=Report.select(
+            result = Report.select(
                 _filters=[Report.create_time.between(beg_time, end_time)])
 
         return self.succ(result)
-
 
     def post(self):
         submit = self.request()
 
         if not submit.__contains__("create_time"):
             submit["create_time"] = datetime.now()
+        else:
+            submit["create_time"] = datetime.strptime(submit["create_time"], "%Y-%m-%d")
 
-        beg_time = (submit["create_time"]-timedelta(days=submit["create_time"].weekday())).replace(hour=0,minute=0,second=0)
-        end_time = (submit["create_time"] + timedelta(days=6 - submit["create_time"].weekday())).replace(hour=23, minute=59, second=59)
+        beg_time = (submit["create_time"] - timedelta(days=submit["create_time"].weekday())).replace(hour=0, minute=0,
+                                                                                                     second=0)
+        end_time = (submit["create_time"] + timedelta(days=6 - submit["create_time"].weekday())).replace(hour=23,
+                                                                                                         minute=59,
+                                                                                                         second=59)
 
-        if Report.query.filter(Report.create_time.between(beg_time,end_time))\
-                .filter(Report.team_id==submit["team_id"]).count():
+        if Report.query.filter(Report.create_time.between(beg_time, end_time)) \
+                .filter(Report.team_id == submit["team_id"]).count():
             raise ExceptionEx("本周计划已添加")
 
         Report.add(submit)
