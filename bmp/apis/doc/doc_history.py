d8c0664ca92f58b3d66819df025bbca90f98668a
CrazyBug <chenglong_yan@foxmail.com>
2016-07-28 13:43:02 +0800

    1.修改 根据doc id 返回历史记录

--- bmp/apis/doc/doc_history.py
+++ bmp/apis/doc/doc_history.py
@@ -5,11 +5,17 @@ from bmp.models.doc import  DocHistory
 
 
 class Doc_historyApi(BaseApi):
-    route = ["/doc/history", "/doc/history/<int:page>/<int:pre_page>", "/doc/history/<int:hid>"]
+    route = ["/doc/history/",
+             "/doc/history/<int:did>/<int:page>/<int:pre_page>",
+             "/doc/history/<int:did>"]
 
-    def get(self, page=None, pre_page=None, hid=None):
-        if hid:
-            return self.succ(DocHistory.get(hid))
+    def get(self, page=None, pre_page=None, did=None):
+        if did:
+            return self.succ(DocHistory.select(
+                page=page,
+                pre_page=pre_page,
+                _filters=DocHistory.doc_id == did
+            ))
         return self.succ(DocHistory.select(
             page,
             pre_page
