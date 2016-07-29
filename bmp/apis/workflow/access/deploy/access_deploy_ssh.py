f4771b62ee782a406ae1d9254427cd582d549f8b
CrazyBug <chenglong_yan@foxmail.com>
2016-07-29 17:35:13 +0800

    1.修改 与权限申请无关独立的创建接口

--- bmp/apis/workflow/access/deploy/access_deploy_ssh.py
+++ bmp/apis/workflow/access/deploy/access_deploy_ssh.py
@@ -1,26 +1,45 @@
 # coding=utf-8
+
 import json
+from datetime import datetime
 
 from bmp import db
 from bmp.apis.base import BaseApi
-from bmp.const import ACCESS
-from bmp.models.access import Access
+from bmp.apis.workflow.access.deploy.access_deploy_history import AccessDeployHistory
+from bmp.const import USER_SESSION
 from bmp.tasks.access import DeploySsh
+from bmp.utils.exception import ExceptionEx
+from flask import session
 
 
 class Access_deploy_sshApi(BaseApi):
-    route = ["/access/deploy/ssh/<int:aid>"]
+    route = ["/access/deploy/ssh"]
 
-    def put(self, aid):
-        access = Access.get(aid)
-        content = json.loads(access["content"])
+    def post(self):
+        submit = self.request()
+        typ = submit.pop("type")
+        detail = json.dumps(submit)
 
-        access["status"] = ACCESS.DEPLOY
-        Access.edit(access, auto_commit=False)
+        ssh = DeploySsh()
 
-        deploy = DeploySsh()
-        if not deploy.add(content):
-            return self.fail()
+        AccessDeployHistory.add(
+            {
+                "create_uid": session[USER_SESSION]["uid"],
+                "create_time": datetime.now(),
+                "type": typ,
+                "detail": detail
+            }, auto_commit=False)
+
+        ret = ssh.add(submit)
+
+        if "ok" not in ret:
+            raise ExceptionEx("添加失败" % ret)
 
         db.session.commit()
         return self.succ()
+
+
+if __name__ == "__main__":
+    ssh = DeploySsh(timeout=None)
+    print ssh.add({"user_name": "chenglong.yan", "hosts": ["192.168.250.111"], "gen_key": "1", "role": "root"})
+
