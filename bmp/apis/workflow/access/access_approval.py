bbc984144eda9c2242d2ce6532771d00e8bee9b9
CrazyBug <chenglong_yan@foxmail.com>
2016-08-16 12:24:06 +0800

    .

--- bmp/apis/workflow/access/access_approval.py
+++ bmp/apis/workflow/access/access_approval.py
@@ -2,11 +2,15 @@
 from datetime import datetime
 
 from bmp.apis.base import BaseApi
-from bmp.models.access import Access
 from bmp.const import ACCESS
+from bmp.models.access import Access
+from bmp.utils.exception import ExceptionEx
+from flask import session
+from bmp.const import USER_SESSION
+
 
 class Access_approvalApi(BaseApi):
-    route = ["/access/approval/<int:aid>","/access/approval/<int:page>/<int:pre_page>"]
+    route = ["/access/approval/<int:aid>", "/access/approval/<int:page>/<int:pre_page>"]
 
     # 申请人 申请时间 类型 理由 内容 操作
     def get(self, page=0, pre_page=None):
@@ -18,6 +22,11 @@ class Access_approvalApi(BaseApi):
 
     def put(self, aid):
         submit = self.request()
+        access = Access.get(aid)
+        if access["status"] != ACCESS.APPROVAL:
+            raise ExceptionEx("申请未提交")
+
+        submit["approval_uid"] = session[USER_SESSION]["uid"]
         submit["approval_time"] = datetime.now()
         submit["id"] = aid
         Access.edit(submit)
--- bmp/tasks/mail/access.py
+++ bmp/tasks/mail/access.py
@@ -4,6 +4,11 @@ from base import BaseMail
 from bmp.utils.user_ldap import Ldap
 import json
 import traceback
+# coding: utf-8
+
+from bmp.models.user import Group, User
+from bmp.const import DEFAULT_GROUP
+from bmp.tasks.mail.base import BaseMail
 
 
 class Mail(BaseMail):
@@ -11,9 +16,12 @@ class Mail(BaseMail):
         try:
             ldap = Ldap()
 
-            copy_to_uid = access.copy_to_uid.split(",") \
+            copy_to_uid = [User.get(uid)["mail"] for uid in access.copy_to_uid.split(",")] \
                 if access.copy_to_uid \
                 else ldap.get_superior(access.apply_uid, inc_1st=True)
+
+            copy_to_uid.extend([u.mail for u in Group.get_users(DEFAULT_GROUP.OP)])
+
 
             sub = u" %s权限申请 申请人:%s 申请时间:%s" % (
                 access.type,
