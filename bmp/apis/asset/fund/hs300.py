059feed77f939670804aae7b2ec81d0c226a3d8f
CrazyBug <chenglong_yan@foxmail.com>
2016-08-19 14:55:05 +0800

    1.新增  end_date 默认当前时间

--- bmp/apis/asset/fund/hs300.py
+++ bmp/apis/asset/fund/hs300.py
@@ -1,22 +1,23 @@
 # coding: utf-8
 
 import csf
-
+from datetime import datetime
 from bmp.apis.base import BaseApi
 
 
 class Hs300Api(BaseApi):
     route = [
-        "/asset/fund/hs300/<string:start_date>/<string:end_date>"
+        "/asset/fund/hs300/<string:start_date>/<string:end_date>",
+        "/asset/fund/hs300/<string:start_date>"
     ]
 
-    def get(self, start_date, end_date):
+    def get(self, start_date, end_date=None):
         csf.config.set_token("03255a3fbddc4c43fe5f01dc7a06f118", "6ZN2oID+6KHqZx68JpmTUDPFWRU=")
         csf.config.debug=True
         bars = csf.get_index_hist_bar(
             "000300",
             start_date=start_date,
-            end_date=end_date,
+            end_date=end_date if end_date else datetime.now().strftime("%Y-%m-%d"),
             field=["date", "close"]
         ).reset_index().rename(columns={"index": "date"})
 
