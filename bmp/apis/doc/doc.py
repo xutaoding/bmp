4d105f4a435c064cf7bd8e78b66d9fc0160d7645
CrazyBug <chenglong_yan@foxmail.com>
2016-08-24 15:52:37 +0800

    1.新增 维护人、业务人的错误提示

--- bmp/apis/doc/doc.py
+++ bmp/apis/doc/doc.py
@@ -2,12 +2,15 @@
 
 from datetime import datetime
 
+from flask import session
+
 from bmp import db
 from bmp.apis.doc.base import BaseDocApi
 from bmp.const import DOC, USER_SESSION
 from bmp.database import Database
 from bmp.models.doc import Doc, DocIndex, DocField
-from flask import session
+from bmp.models.user import User
+from bmp.utils.exception import ExceptionEx
 
 
 class DocApi(BaseDocApi):
@@ -32,6 +35,15 @@ class DocApi(BaseDocApi):
         submit["modify_time"] = datetime.now()
         submit["modify_uid"] = session[USER_SESSION]["uid"]
 
+        uids = User.uids()
+        if submit.__contains__("mainten_uid"):
+            if submit["mainten_uid"] not in uids:
+                raise ExceptionEx("不存在维护人 %s" % submit["mainten_uid"])
+
+        if submit.__contains__("opt_uid"):
+            if submit["opt_uid"] not in uids:
+                raise ExceptionEx("不存在业务人 %s" % submit["opt_uid"])
+
         doc = Doc.add(submit, auto_commit=False)
 
         doc.indexs = [Database.to_cls(DocIndex, index) for index in indexs]
@@ -45,6 +57,16 @@ class DocApi(BaseDocApi):
 
         indexs = submit.pop("indexs") if submit.__contains__("indexs") else None
         fields = submit.pop("fields") if submit.__contains__("fields") else None
+
+        uids = User.uids()
+        if submit.__contains__("mainten_uid"):
+            if submit["mainten_uid"] not in uids:
+                raise ExceptionEx("不存在维护人 %s" % submit["mainten_uid"])
+
+        if submit.__contains__("opt_uid"):
+            if submit["opt_uid"] not in uids:
+                raise ExceptionEx("不存在业务人 %s" % submit["opt_uid"])
+
 
         submit["id"] = did
         doc = Doc.edit(submit, auto_commit=False)
